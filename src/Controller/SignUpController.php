2ba48cd35b622df164c0c1d59157de452e81180f
root <tatyana.merlo@gmail.com>
2016-05-31 14:17:44 +0200

    sign up hashing

--- src/Controller/SignUpController.php
+++ src/Controller/SignUpController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace tippTopf\src\Controller; 
 
 use Psr\Http\Message\ServerRequestInterface;
@@ -26,7 +25,7 @@ class SignUpController {
     public function signUp(ServerRequestInterface $request, ResponseInterface $response) {
         $app = $this->app;      
         HelperController::getView($app);
-        
+              
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $db = HelperController::getConnection();
             
@@ -42,11 +41,15 @@ class SignUpController {
             $stmt = $db->prepare($sql);
             $stmt->bindParam(':username', $_POST["username"]);
             $stmt->execute();
-            $result_username = $stmt->fetchAll();
-            
-            if (empty($result_mail) && empty($result_username)){
+            $result_username = $stmt->fetchAll();                        
+                                  
+            if (empty($result_mail) && empty($result_username)){   
                 $this->addNewUser($_POST, $db);
+            } else {
+                $app->getContainer()['view']->render($response, 'signup.html.twig', array('title' => 'Sign Up', 'page_title' => 'Sign Up'));
             }
+            
+            $app->getContainer()['view']->render($response, 'home.html.twig', array('title' => 'Home', 'page_title' => 'Home - Thanks for Signing Up!'));
 
         } else  {
             $app->getContainer()['view']->render($response, 'signup.html.twig', array('title' => 'Sign Up', 'page_title' => 'Sign Up'));
@@ -54,17 +57,24 @@ class SignUpController {
     }
     
     /**
-     * add new user into db
+     * hash password & add new user into db
      * @param array $user
      */
     private function addNewUser($user, $db){
-        $sql = "INSERT INTO mydb.user(user.username, user.password,user.email) 
+                      
+        $options = [
+            'cost' => 11,
+            'salt' => mcrypt_create_iv(22, MCRYPT_DEV_URANDOM),
+        ];
+        $hash =  password_hash($user["password"], PASSWORD_BCRYPT, $options); 
+        
+        $sql = "INSERT INTO user(username, password, email) 
             VALUES (:username, :password, :email)";            
 
         $stmt = $db->prepare($sql);
         $stmt->bindParam(':username', $user["username"]);
-        $stmt->bindParam(':password', $user["password"]);
-        $stmt->bindParam(':password', $user["email"]);
+        $stmt->bindParam(':password', $hash);
+        $stmt->bindParam(':email', $user["email"]);
         $stmt->execute();
     }
 
