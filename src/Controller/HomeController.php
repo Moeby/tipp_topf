0e9132bcd42aaa796af11f0fdf2b88c7c1952045
root <tatyana.merlo@gmail.com>
2016-06-12 14:00:27 +0200

    startpage different for logged in and logged out users

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -27,28 +27,32 @@ class HomeController {
         $app = $this->app;      
         $this->app->getContainer()['view']->getEnvironment()->addGlobal("session", $_SESSION);
         
-        $db = HelperController::getConnection();
-        
-        //check if user has been invited to a game
-        $email = HelperController::getLoggedInUserMail();
-        $user_id = HelperController::getLoggedInUserId();
-        
-        $sql = "SELECT * FROM mydb.invitation WHERE invitation.email = :email";
+        if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
 
-        $stmt = $db->prepare($sql);
-        $stmt->bindParam(':email', $email);
-        $stmt->execute();
-        $invitations = $stmt->fetchAll();
-        
-        //add group name to array
-        if (!empty($invitations)){
-            foreach ($invitations as $key => $invitation){
-                $group = HelperController::getGroup($invitation["group_id"]);
-                $invitations[$key]['groupname'] = $group["name"];
+            $db = HelperController::getConnection();
+
+            //check if user has been invited to a game
+            $email = HelperController::getLoggedInUserMail();
+            $user_id = HelperController::getLoggedInUserId();
+
+            $sql = "SELECT * FROM mydb.invitation WHERE invitation.email = :email";
+
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(':email', $email);
+            $stmt->execute();
+            $invitations = $stmt->fetchAll();
+
+            //add group name to array
+            if (!empty($invitations)){
+                foreach ($invitations as $key => $invitation){
+                    $group = HelperController::getGroup($invitation["group_id"]);
+                    $invitations[$key]['groupname'] = $group["name"];
+                }   
             }
+            $app->getContainer()['view']->render($response, 'home.html.twig', array('title' => 'Home', 'page_title' => 'TippTopf', 'invitations' => $invitations[0], 'user_id' => $user_id));
+        } else {
+            $app->getContainer()['view']->render($response, 'HomeLoggedOut.html.twig', array('title' => 'Home', 'page_title' => 'TippTopf'));
         }
-        
-        $app->getContainer()['view']->render($response, 'home.html.twig', array('title' => 'Home', 'page_title' => 'TippTopf', 'invitations' => $invitations[0], 'user_id' => $user_id));
     }
     
 }
